c6d99cd84b2af4c2719659589b34b862146397cf
erjan123 <erjan123@yahoo.com>
2018-12-03 22:03:29 +0200

    Microsoft Asp.NET Core Web Api Todo example

---
+++ TodoApiAspNetCoreWebApi/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApiAspNetCoreWebApi.Models
+{
+    public class TodoContext: DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
--- TodoApiAspNetCoreWebApi/Startup.cs
+++ TodoApiAspNetCoreWebApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using TodoApiAspNetCoreWebApi.Models;
 
 namespace TodoApiAspNetCoreWebApi
 {
@@ -25,6 +27,7 @@ namespace TodoApiAspNetCoreWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -40,6 +43,8 @@ namespace TodoApiAspNetCoreWebApi
                 app.UseHsts();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
